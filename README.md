b2f091310ca6f127c7d043f9355d2bc57e274bc1
Sean Massa <endangeredmassa@gmail.com>
2024-08-08 12:46:13 -0500

    some improvements

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-# Slack Emoji Scraper
+# Slack Emoji Exporter
 
-- Put your emoji.json in this folder.
-- From this directory, run in your terminal:
+- Go to "Customize Your Workspace"
+- Open dev tools > Network tab
+- Search for `adminList`
+- Copy Value > Copy as cURL
+- paste into a text buffer, then change `adminList` to `list`
+- run the curl command, but output it to a file `... > emojis.json`
+- move the `emojis.json` file into the root of this project
+
+Then run:
 
 ```
-node grab-emoji <emoji.json filename>
+pnpm download emojis.json
 ```
+
+This will download all of the emoji in that list into the `./output` directory. It operates one file at a time to be kind to Slack's servers.
--- grab-emoji.js
+++ grab-emoji.js
@@ -1,26 +1,45 @@
-if (!process.argv[2]) {
-  console.log('You forgot the filename!');
-  console.log('(╯°□°)╯︵ ┻━┻ ');
-  process.exit();
-}
-const emojis = require(`./${process.argv[2]}`).emoji;
 const fs = require('fs');
 const https = require('https');
-console.log('Emoji', emojis.length);
-for (emoji of emojis) {
-  const extension = emoji.url.split('.').pop();
-  const file = fs.createWriteStream(`./output/${emoji.name}.${extension}`);
-  const request = https
-    .get(emoji.url, function (response) {
-      response.pipe(file);
 
-      // after download completed close filestream
-      file.on('finish', () => {
-        file.close();
-        // console.log('Download Completed');
+async function download(name, url) {
+  return new Promise((resolve, reject) => {
+    const extension = url.split('.').pop();
+    const file = fs.createWriteStream(`./output/${name}.${extension}`);
+    
+    https
+      .get(url, function (response) {
+        response.pipe(file);
+        file.on('finish', () => {
+          file.close();
+          resolve();
+        });
+      })
+      .on('error', error => {
+        console.warn(`Failed to download ${url}: `, error.message);
+        reject(error);
       });
-    })
-    .on('error', e => {
-      console.warn('error!', e);
-    });
+  });
 }
+
+async function doWork() {
+  const filename = process.argv[2];
+  if (!filename) {
+    console.log('You forgot the filename!');
+    console.log('(╯°□°)╯︵ ┻━┻ ');
+    process.exit();
+  }
+  
+  const emojis = require(`./${filename}`).emoji;
+  
+  console.log(`Downloading ${Object.entries(emojis).length} emoji from Slack`);
+  
+  for (let [name, url] of Object.entries(emojis)) {
+    if (!url.startsWith('alias:')) {
+      await download(name, url);
+    }
+  }
+  
+  console.log('All done!');
+}
+
+doWork().catch(console.error);
